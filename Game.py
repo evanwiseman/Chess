e6e52422569182bfae9118d3cb3fd8b21dc23997
Evan Wiseman <evanwiseman2000@gmail.com>
2024-02-11 22:07:46 -0800

    Reformatted

--- Game.py
+++ Game.py
@@ -2,11 +2,13 @@ from Board import Board
 from Player import Player
 
 class Game:
-    def __init__(self):
+    def __init__(self) -> None:
         self.player1 = Player()
         self.player2 = Player()
         self.board = Board()
-            
+        
+        return
 
 if __name__ == "__main__":
-    print("starting game")
+    print("starting game...")
+    game = Game()
